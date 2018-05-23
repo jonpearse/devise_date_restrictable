3f6b4e25ebc11f22f1bdb205849e7759f9db3264
Jon Pearse <jon@jonpearse.net>
2018-05-23 15:25:55 +0100

    Adding ActiveRecord::Migration version to the the migration template (fixes #4)

--- lib/generators/devise_date_restrictable/templates/migration.rb
+++ lib/generators/devise_date_restrictable/templates/migration.rb
@@ -1,9 +1,9 @@
-class DeviseAdd<%= @model_name.camelize.singularize %>DateRestrictionFields < ActiveRecord::Migration
+class DeviseAdd<%= @model_name.camelize.singularize %>DateRestrictionFields < ActiveRecord::Migration[<%= ActiveRecord::Migration.current_version %>]
 
  def change
 
-  add_column :<%=@model_name.tableize%>, :valid_from, :date
-  add_column :<%=@model_name.tableize%>, :valid_until, :date
+  add_column :<%=@model_name.tableize %>, :valid_from, :date
+  add_column :<%=@model_name.tableize %>, :valid_until, :date
 
  end
 
